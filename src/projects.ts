75628a989aa4c0490d982b247e12b4142f2531d1
varshith pilli <varshith6946@gmail.com>
2025-10-31 10:11:22 +0530

    day2

--- src/projects.ts
+++
@@ -1,6 +0,0 @@
-export const projects = [
-  { name: "Project 1", path: "project1" },
-  { name: "Project 2", path: "project2" },
-  { name: "Project 3", path: "project3" },
-  { name: "Project 4", path: "project4" },
-]
