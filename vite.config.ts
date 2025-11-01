4d7f95cccd4f0dac4f6b31c9046a158b3e34be72
varshith pilli <varshith6946@gmail.com>
2025-11-01 11:28:46 +0530

    deploy-test-03

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "nerdhub/",
+  base: "/nerdhub",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
