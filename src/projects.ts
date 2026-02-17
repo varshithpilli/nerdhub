e464ce9c898bf2d9fe1207eb4f704894f69ad21b
varshith pilli <varshith6946@gmail.com>
2026-02-18 02:34:01 +0530

    rm some stuff

--- src/projects.ts
+++ src/projects.ts
@@ -24,26 +24,26 @@ export const projects = [
         path: "https://varzone.in/bars/",
         description: "Lyrics to live by."
     },
-    {
-        name: "Lin-alg",
-        path: "https://varzone.in/Lin-Alg/",
-        description: "Made it to increase quiz marks."
-    },
-    {
-        name: "Calender",
-        path: "https://varzone.in/calendar-events/",
-        description: "A simple HTML + CSS calender with event managing."
-    },
-    {
-        name: "Notes",
-        path: "https://varzone.in/notes-app/",
-        description: "A small React attempt to make a note taking app."
-    },
-    {
-        name: "Quiz",
-        path: "https://varzone.in/quiz-app/",
-        description: "A simple HTML + CSS static quiz app."
-    },
+    // {
+    //     name: "Lin-alg",
+    //     path: "https://varzone.in/Lin-Alg/",
+    //     description: "Made it to increase quiz marks."
+    // },
+    // {
+    //     name: "Calender",
+    //     path: "https://varzone.in/calendar-events/",
+    //     description: "A simple HTML + CSS calender with event managing."
+    // },
+    // {
+    //     name: "Notes",
+    //     path: "https://varzone.in/notes-app/",
+    //     description: "A small React attempt to make a note taking app."
+    // },
+    // {
+    //     name: "Quiz",
+    //     path: "https://varzone.in/quiz-app/",
+    //     description: "A simple HTML + CSS static quiz app."
+    // },
     {
         name: "Breach",
         path: "https://github.com/varshithpilli/breached-data",
